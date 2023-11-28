003c5f1e1ca15645f05b0569c4e05fe53bb7602c
volkan <16579273+mevolkan@users.noreply.github.com>
2023-11-28 21:50:44 +0300

    navigation mockup

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <div>
+
+            </div>
+            <div>
+                <h1>Bitcoin Wallet</h1>
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    )
+}
