dd356fe63ddeaaf45843140ccb4a16c35e21224f
volkan <16579273+mevolkan@users.noreply.github.com>
2023-11-28 22:54:26 +0300

    time filter markup

---
+++ src/components/timeFilter.jsx
@@ -0,0 +1,11 @@
+
+export default function TimeFilter() {
+    return (
+        <div className='time-filter'>
+          <span>Day</span>
+          <span className="active">Week</span>
+          <span>Month</span>
+          <span>Year</span>
+        </div>
+    )
+}
