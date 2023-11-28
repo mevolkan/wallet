99edc0acf72255eb5cb2aa0990dcccf80eb38549
volkan <16579273+mevolkan@users.noreply.github.com>
2023-11-28 22:25:50 +0300

    navbar icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navigation from './components/navbar'
+
 import './App.css'
+import Navigation from './components/navbar'
 
 function App() {
+
 
   return (
     <>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,22 @@
+import React from 'react';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronLeft,faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faChevronLeft,faEllipsisVertical);
+
 export default function Navigation() {
     return (
         <nav>
             <div>
-
+            <FontAwesomeIcon icon={faChevronLeft} />
             </div>
             <div>
                 <h1>Bitcoin Wallet</h1>
             </div>
             <div>
-
+            <FontAwesomeIcon icon={faEllipsisVertical} />
             </div>
         </nav>
     )
